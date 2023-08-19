8bb58e862e20e56934be9188acfc34ae9a18a1a6
Choudhury555 <choudhurypanda143@gmail.com>
2023-08-19 23:22:41 +0530

    removing some css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from "react"
 import Header from "./components/Header"
 import Meme from "./components/Meme"
 import "./App.css"
-
 export default function App() {
     return (
-        <div className="sizesetup">
+        <div>
             <Header />
-            <div className="mid">
-                <Meme />
-            </div>
+            <Meme />
         </div>
     )
 }
